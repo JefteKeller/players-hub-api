d545e68b85bf8fcba7189235ddfbb68913658b48
Guilherme Stavis <guistavis@gmail.com>
2021-04-14 20:29:51 -0300

    feat: added create command for all registered tables

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,8 @@
-from .helpers import populate_users
+from .helpers import (
+    populate_users,
+    populate_teams,
+    populate_games,
+    populate_matches,
+    populate_locations,
+    populate_teams_users,
+)
--- app/services/helpers.py
+++ app/services/helpers.py
@@ -1,3 +1,7 @@
+from faker import Faker
+
+fake = Faker()
+
 populate_users = [
     {
         "nickname": "ZezinhoGameplays23",
@@ -5,21 +9,7 @@ populate_users = [
         "last_name": "Neves",
         "email": "bebeto@gmail.com",
         "password": "senh@123",
-        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
-                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
-                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
-                      dolor blandit eget. Morbi eleifend erat non eros congue
-                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
-                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
-                      Phasellus pharetra elit gravida velit pellentesque aliquet.
-                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
-                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
-                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
-                      bibendum. Proin molestie lacus eu tellus euismod dictum.
-                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
-                      enim. Vestibulum sodales lobortis lobortis.""",
+        "biography": fake.text()[:100],
     },
     {
         "nickname": "AlbertoRoberto",
@@ -27,21 +17,7 @@ populate_users = [
         "last_name": "de Abreu",
         "email": "zezedeabreu@hotmail.com",
         "password": "senh@123",
-        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
-                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
-                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
-                      dolor blandit eget. Morbi eleifend erat non eros congue
-                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
-                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
-                      Phasellus pharetra elit gravida velit pellentesque aliquet.
-                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
-                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
-                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
-                      bibendum. Proin molestie lacus eu tellus euismod dictum.
-                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
-                      enim. Vestibulum sodales lobortis lobortis.""",
+        "biography": fake.text()[:100],
     },
     {
         "nickname": "OMamaGanso",
@@ -49,21 +25,7 @@ populate_users = [
         "last_name": "Junior",
         "email": "juninho@gmail.com",
         "password": "senh@123",
-        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
-                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
-                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
-                      dolor blandit eget. Morbi eleifend erat non eros congue
-                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
-                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
-                      Phasellus pharetra elit gravida velit pellentesque aliquet.
-                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
-                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
-                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
-                      bibendum. Proin molestie lacus eu tellus euismod dictum.
-                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
-                      enim. Vestibulum sodales lobortis lobortis.""",
+        "biography": fake.text()[:100],
     },
     {
         "nickname": "PrincessDuMal",
@@ -71,20 +33,129 @@ populate_users = [
         "last_name": "Almeida",
         "email": "belaalmeida@yahoo.com",
         "password": "senh@123",
-        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
-                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
-                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
-                      dolor blandit eget. Morbi eleifend erat non eros congue
-                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
-                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
-                      Phasellus pharetra elit gravida velit pellentesque aliquet.
-                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
-                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
-                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
-                      bibendum. Proin molestie lacus eu tellus euismod dictum.
-                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
-                      enim. Vestibulum sodales lobortis lobortis.""",
+        "biography": fake.text()[:100],
+    },
+]
+
+populate_teams = [
+    {
+        "team_name": "Os Brabíssimos",
+        "team_description": fake.text()[:50],
+        "owner_id": 1,
+    },
+    {
+        "team_name": "Chama no dale",
+        "team_description": fake.text()[:50],
+        "owner_id": 2,
+    },
+    {
+        "team_name": "Xesquedele",
+        "team_description": fake.text()[:50],
+        "owner_id": 3,
+    },
+    {
+        "team_name": "Fogo no Parquinho",
+        "team_description": fake.text()[:50],
+        "owner_id": 4,
+    },
+]
+
+populate_games = [
+    {
+        "game_name": "League of Legends",
+        "game_type": "Online",
+        "game_description": fake.text()[:50],
+    },
+    {
+        "game_name": "Futebol",
+        "game_type": "Físico",
+        "game_description": fake.text()[:50],
+    },
+    {
+        "game_name": "Xadrez",
+        "game_type": "Físico",
+        "game_description": fake.text()[:50],
+    },
+]
+
+populate_matches = [
+    {
+        "match_winner": "team1",
+        "team_id_1": 1,
+        "team_id_2": 2,
+        "date": "06/06/21",
+        "game_id": 1,
+    },
+    {
+        "match_winner": "team3",
+        "team_id_1": 1,
+        "team_id_2": 3,
+        "date": "25/04/21",
+        "game_id": 3,
+    },
+    {
+        "match_winner": "team1",
+        "team_id_1": 1,
+        "team_id_2": 4,
+        "date": "30/03/21",
+        "game_id": 3,
+    },
+    {
+        "match_winner": "team3",
+        "team_id_1": 2,
+        "team_id_2": 3,
+        "date": "24/10/21",
+        "game_id": 1,
+    },
+    {
+        "match_winner": "team2",
+        "team_id_1": 2,
+        "team_id_2": 4,
+        "date": "12/01/21",
+        "game_id": 2,
+    },
+    {
+        "match_winner": "team4",
+        "team_id_1": 3,
+        "team_id_2": 4,
+        "date": "05/04/21",
+        "game_id": 1,
+    },
+]
+
+populate_locations = [
+    {
+        "location_name": "Sua casa",
+        "location_phone": "Seu Telefone",
+        "match_id": 1,
+    },
+    {
+        "location_name": "Campo do Joquinha",
+        "location_phone": "(41) 3558-4178",
+        "match_id": 5,
+    },
+    {
+        "location_name": "Clube de Xadrez do Jefte",
+        "location_phone": "(41) 98514-1317",
+        "match_id": 2,
+    },
+]
+
+populate_teams_users = [
+    {
+        "user_id": 1,
+        "team_id": 2,
+    },
+    {
+        "user_id": 3,
+        "team_id": 3,
+    },
+    {
+        "user_id": 3,
+        "team_id": 1,
+    },
+    {
+        "user_id": 4,
+        "team_id": 4,
     },
 ]
