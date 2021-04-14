81fa425df4c39851fdfa88a4cfd1324b431a89d1
Guilherme Stavis <guistavis@gmail.com>
2021-04-14 19:02:15 -0300

    add migration settings and populate users table comand

--- app/configurations/commands.py
+++ app/configurations/commands.py
@@ -1,16 +1,39 @@
 from flask import Flask
 from flask.cli import AppGroup
 
+from ..services import populate_users
+from ..models import UserModel
+
 
 def init_app(app: Flask):
-    cli_db_group = AppGroup('db_cli')
+    session = app.db.session
+    cli_db_group = AppGroup("db_cli")
 
-    @cli_db_group.command('create')
+    @cli_db_group.command("create")
     def cli_db_create_all():
         app.db.create_all()
-    
-    @cli_db_group.command('drop')
+
+    @cli_db_group.command("drop")
     def cli_db_drop_all():
         app.db.drop_all()
 
     app.cli.add_command(cli_db_group)
+
+    cli_user_group = AppGroup("user")
+
+    @cli_user_group.command("create")
+    def cli_user_create():
+        for user in populate_users:
+            new_user = UserModel(
+                nickname=user["nickname"],
+                first_name=user["first_name"],
+                last_name=user["last_name"],
+                email=user["email"],
+                password=user["password"],
+                biography=user["biography"],
+            )
+
+            session.add(new_user)
+            session.commit()
+
+    app.cli.add_command(cli_user_group)
---
+++ app/services/helpers.py
@@ -0,0 +1,90 @@
+populate_users = [
+    {
+        "nickname": "ZezinhoGameplays23",
+        "first_name": "Alberto",
+        "last_name": "Neves",
+        "email": "bebeto@gmail.com",
+        "password": "senh@123",
+        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
+                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
+                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
+                      dolor blandit eget. Morbi eleifend erat non eros congue
+                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
+                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
+                      Phasellus pharetra elit gravida velit pellentesque aliquet.
+                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
+                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
+                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
+                      bibendum. Proin molestie lacus eu tellus euismod dictum.
+                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
+                      enim. Vestibulum sodales lobortis lobortis.""",
+    },
+    {
+        "nickname": "AlbertoRoberto",
+        "first_name": "José",
+        "last_name": "de Abreu",
+        "email": "zezedeabreu@hotmail.com",
+        "password": "senh@123",
+        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
+                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
+                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
+                      dolor blandit eget. Morbi eleifend erat non eros congue
+                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
+                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
+                      Phasellus pharetra elit gravida velit pellentesque aliquet.
+                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
+                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
+                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
+                      bibendum. Proin molestie lacus eu tellus euismod dictum.
+                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
+                      enim. Vestibulum sodales lobortis lobortis.""",
+    },
+    {
+        "nickname": "OMamaGanso",
+        "first_name": "Patrick",
+        "last_name": "Junior",
+        "email": "juninho@gmail.com",
+        "password": "senh@123",
+        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
+                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
+                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
+                      dolor blandit eget. Morbi eleifend erat non eros congue
+                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
+                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
+                      Phasellus pharetra elit gravida velit pellentesque aliquet.
+                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
+                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
+                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
+                      bibendum. Proin molestie lacus eu tellus euismod dictum.
+                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
+                      enim. Vestibulum sodales lobortis lobortis.""",
+    },
+    {
+        "nickname": "PrincessDuMal",
+        "first_name": "Isabela",
+        "last_name": "Almeida",
+        "email": "belaalmeida@yahoo.com",
+        "password": "senh@123",
+        "biography": """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nunc vestibulum nulla vitae arcu tincidunt imperdiet.
+                      Duis metus mi, placerat in lacus nec, fringilla ullamcorper
+                      nulla. Pellentesque placerat justo ex, vitae ullamcorper
+                      dolor blandit eget. Morbi eleifend erat non eros congue
+                      lobortis. Pellentesque a cursus orci, cursus egestas sapien.
+                      Quisque non diam tristique, dapibus ante quis, consectetur urna.
+                      Phasellus pharetra elit gravida velit pellentesque aliquet.
+                      Aenean pharetra dolor vitae dapibus ultricies. Vestibulum ante
+                      ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                      curae; Curabitur ullamcorper ultrices velit vel tincidunt.
+                      Nulla bibendum dolor convallis enim aliquet, non cursus sapien
+                      bibendum. Proin molestie lacus eu tellus euismod dictum.
+                      Fusce vestibulum sit amet arcu eu pharetra. Ut eget convallis
+                      enim. Vestibulum sodales lobortis lobortis.""",
+    },
+]
