26cbbbdb24b1ef5f729955a2837021fdf5c80788
Felipe Sobrinho Molina <felipe16sm@gmail.com>
2021-04-22 23:36:44 -0300

    fix: fixed bug in matches endpoint.

--- app/serializers/match_serializer.py
+++ app/serializers/match_serializer.py
@@ -2,10 +2,8 @@ from app.models.match_model import MatchModel
 
 
 def match_serializer(match):
-
     return {
         "game": match.game.game_name,
-        "match_winner": match.match_winner.team_name,
         "date": match.date,
         "team_1": match.team_1.team_name,
         "team_2": match.team_2.team_name,
